6ea6dfaeeb40940232304615d4cb6a2405813f4e
Iris Lux <61400698+iris-lux@users.noreply.github.com>
2020-03-30 14:59:35 -0700

    Update ada_application_problem-1.rb

--- ada_application_problem-1.rb
+++ ada_application_problem-1.rb
@@ -1,24 +1,8 @@
- #In Ruby, csv is not included by defaul - in order to use CSV classes in Ruby, you must "Require CSV"
+#Load csv library
 require 'csv'
-#parse (looks up how that
-#CSV row is like an array and has had a baby, I can reference a value in the row by its ?index? or by its header
-#Creating the table object
+#Load CSV file into CSV:Table object, with headers
 employee_earnings_table = CSV.parse(File.read("C:/Ruby/ada/City_of_Albany_Employee_Earnings_2012.csv"), headers: true)
-
-# -Look up block
-#
-
+#Filter the table so it does not contain names the name “JOHN”, employees who made zero overtime, or any “BUILDINGS AND COMPLIANCE” employees
 employee_earnings_table.delete_if{|row| row["OVERTIME EARNINGS"].to_f == 0 || row["EMPLOYEE NAME"].include?("JOHN") || row["DEPARTMENT NAME"] == "BUILDINGS AND COMPLIANCE"}
-
-
-
-
-#Getting the top value was tricky - less elegant than looking at it. It's easy to sort the Regular Earnings, but it was more difficult to determine which name value corresponded to the highest Regular EARNINGS
-# == two values or two objects
-# Break down explanation by the things I'm comparing
-# "table["REGULAR EARNINGS"].sort.reverse[0]" gives my the highest earning, but then I need to find the "Employee Name" that corresponds to this value
-# Output/Return the name on the row that has the highest value
-#make array out of table.sort.reverse
-
-
+#Output name of the employee with the highest regular earnings
 puts employee_earnings_table.max_by {|row| row["REGULAR EARNINGS"].to_f}["EMPLOYEE NAME"]
