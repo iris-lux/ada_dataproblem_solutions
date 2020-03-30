84d6f67d7dbe511c26cb14ddbcebeef4bffb80ac
Iris Lux <61400698+iris-lux@users.noreply.github.com>
2020-03-30 15:04:28 -0700

    Update ada_application_problem-3.rb

--- ada_application_problem-3.rb
+++ ada_application_problem-3.rb
@@ -6,5 +6,5 @@ employee_earnings_table = CSV.parse(File.read("C:/Ruby/ada/City_of_Albany_Employ
 employee_earnings_table.delete_if{|row| row["OVERTIME EARNINGS"].to_f == 0 || row["EMPLOYEE NAME"].include?("JOHN") || row["DEPARTMENT NAME"] == "BUILDINGS AND COMPLIANCE"}
 #Create array of overtime earnings from depts with less than 50 employees
 overtime_earnings = employee_earnings_table.group_by {|row| row["DEPARTMENT NAME"]}.filter{|key, value| value.length > 50}.values.flatten(1).collect{|row| row["OVERTIME EARNINGS"].to_f}.sort
-#OUtput median overtime earnings
+#Output median overtime earnings
 puts ((overtime_earnings[(overtime_earnings.length - 1) / 2] + overtime_earnings[overtime_earnings.length / 2]) / 2)
